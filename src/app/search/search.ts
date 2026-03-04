2caa46584cc1a6c641094279ad4139b37c16735a
Shprotya <S00267553@atu.ie>
2026-03-04 12:25:16 +0000

    search component completed

--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { MovieapiService } from '../movie-apiservice';
+import { FormsModule } from '@angular/forms';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
-export class Search {}
+
+export class Search {
+  private movieService = inject(MovieapiService);
+
+  onSearch(title: string): void {
+    if (title.trim()) {
+      this.movieService.getMovie(title);
+    }
+  }
+}
