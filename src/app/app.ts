a524e28c14130fc470891d5c2cad5afca6d424a2
Shprotya <S00267553@atu.ie>
2026-03-11 11:17:41 +0000

    created components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Search } from './search/search';
 import { Details } from './details/details';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Search, Details],
+  imports: [RouterOutlet, Search, Details, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
