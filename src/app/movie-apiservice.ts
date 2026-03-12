343ed743b3192a3db3aafeb81194b84a960c9fc2
Shprotya <S00267553@atu.ie>
2026-03-12 17:23:44 +0000

    pagination is done

--- src/app/movie-apiservice.ts
+++ src/app/movie-apiservice.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal, computed } from '@angular/core';
 import { take } from 'rxjs';
 import { MovieResults, SearchResults } from './models/moviedetails.interface';
 
@@ -11,29 +11,53 @@ export class MovieapiService {
 
   private readonly _siteURL = "https://www.omdbapi.com/";
   private readonly _key = "4bbb8ce2";
+  private readonly _perPage = 10;
+  private _lastQuery = '';
 
   public movies = signal<MovieResults[] | null>(null);
   public movie = signal<MovieResults | null>(null);
 
-  constructor() { }
+  public totalResults = signal<number>(0);
+  public currentPage = signal<number>(1);
+  public maxPages = computed(() => Math.ceil(this.totalResults() / this._perPage));
 
-  getMovies(title: string) {
-    const url = `${this._siteURL}?s=${title}&apikey=${this._key}`;
+  getMovies(title: string, page: number = 1) {
+    this._lastQuery = title;
+    const url = `${this._siteURL}?s=${title}&page=${page}&apikey=${this._key}`;
 
-    this.http.get<SearchResults>(url)
+    this.http.get<any>(url)
       .pipe(take(1))
       .subscribe({
         next: (data) => {
           if (data.Response === "True") {
             this.movies.set(data.Search);
+            this.totalResults.set(+data.totalResults);  // + converts string to number
+            this.currentPage.set(page);
           } else {
-            this.movies.set([]); 
+            this.movies.set([]);
+            this.totalResults.set(0);
+            this.currentPage.set(1);
           }
         },
         error: (err) => {
-          this.movies.set([]);  
+          console.error(err);
+          this.movies.set([]);
+          this.totalResults.set(0);
+          this.currentPage.set(1);
         }
       });
+  }
+
+  nextPage() {
+    if (this.currentPage() < this.maxPages()) {
+      this.getMovies(this._lastQuery, this.currentPage() + 1);
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage() > 1) {
+      this.getMovies(this._lastQuery, this.currentPage() - 1);
+    }
   }
 
   getMovie(id: string) {
@@ -43,7 +67,7 @@ export class MovieapiService {
       .subscribe({
         next: (data) => this.movie.set(data),
         error: (err) => {
-          console.error('Error fetching movie:', err);
+          console.error(err);
           this.movie.set(null);
         }
       });
